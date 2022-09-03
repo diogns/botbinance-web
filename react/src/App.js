74a453bc4fd4fc31ddf727cc0f8dc200040214e4
root <root@bot-binance-uni-vm.us-central1-c.c.lively-oxide-360715.internal>
2022-09-03 05:47:53 +0000

    first commit

---
+++ react/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+
+export default function App(props) {
+    console.log("props",props)
+
+    return (
+        <h1>Hello world</h1>
+
+    );
+}
+
